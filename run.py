f17af69c1bf39acec0ec9948de3b4e7ed3d637bc
hannes.breitfeld <hannes.breitfeld@fresenius.com>
2025-01-06 13:46:32 +0100

    Update run.py to load environment variables and specify host and port for app execution

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from app import create_app, db
 
+# Load environment variables from a .env file if it exists
 load_dotenv()
 
 config_name = os.getenv('FLASK_CONFIG') or 'development'
@@ -10,4 +11,4 @@ app = create_app(config_name)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run()
+    app.run(host='0.0.0.0', port=8000)  # Ensure the app runs on the correct host and port
