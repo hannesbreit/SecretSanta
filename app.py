52b9e37c974e5404ce385aecc293128fadef42ae
hannes.breitfeld <hannes.breitfeld@fresenius.com>
2025-01-06 15:14:22 +0100

    Update app configuration to production and add gunicorn to requirements

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from app import create_app, db
 # Load environment variables from a .env file if it exists
 load_dotenv()
 
-config_name = os.getenv('FLASK_CONFIG') or 'development'
+config_name = os.getenv('FLASK_CONFIG') or 'production'
 app = create_app(config_name)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8000)  # Ensure the app runs on the correct host and port
+    app.run()  # Ensure the app runs on the correct host and port
